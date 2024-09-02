3eb2b49120306fd844cbf67c65bb308d32eb63ca
Shubham <sk246903@gmail.com>
2024-09-03 01:48:23 +0530

    Added search functionality on the dir tree

--- fos/components/FileExplorer.py
+++ fos/components/FileExplorer.py
@@ -1,8 +1,32 @@
+from pathlib import Path
+from typing import Iterable
+
 from textual.app import ComposeResult
-from textual.widgets import DirectoryTree, Static
+from textual.reactive import reactive
+from textual.widget import Widget
+from textual.widgets import DirectoryTree
 
 
-class FileExplorer(Static):
+class FileExplorer(Widget):
+    search = reactive("", recompose=True)
 
     def compose(self) -> ComposeResult:
-        yield DirectoryTree("./", id="file-ex", classes="border")
+        yield FilteredDirectoryTree("./", search_query=self.search)
+
+
+class FilteredDirectoryTree(DirectoryTree):
+    def __init__(self, path: str | Path, search_query):
+        super().__init__(path)
+        self.search = search_query
+
+    def filter_paths(self, paths: Iterable[Path]) -> Iterable[Path]:
+        def contains_search_term(path: Path) -> bool:
+            if self.search.lower() in path.name.lower():
+                return True
+            if path.is_dir():
+                for subpath in path.iterdir():
+                    if contains_search_term(subpath):
+                        return True
+            return False
+
+        return [path for path in paths if contains_search_term(path)]
--- fos/layout/SidePanel.py
+++ fos/layout/SidePanel.py
@@ -1,10 +1,13 @@
 from textual.app import ComposeResult
-from textual.widgets import Static
+from textual.widgets import Static, Input
 from textual.containers import Container
 from fos.components.SearchBox import SearchBox
 from fos.components.FileExplorer import FileExplorer
 
 
 class SidePanel(Static):
+    def on_input_changed(self, message: Input.Changed) -> None:
+        self.query_one(FileExplorer).search = message.value
+
     def compose(self) -> ComposeResult:
         yield Container(Container(FileExplorer(), SearchBox(), classes="vertical-layout"), classes="horizontal-layout")
